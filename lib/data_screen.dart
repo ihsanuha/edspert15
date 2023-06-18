c525e88c62bbac44f39d8e1ddd7c8a7ceef1df6e
ihsanuha <ihsanuha@gmail.com>
2023-06-18 11:21:27 +0700

    second commit

--- lib/data_screen.dart
+++ lib/data_screen.dart
@@ -7,27 +7,28 @@ class DataScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Column(
-        children: [
-          Image.network(
-              'https://st3.depositphotos.com/9998432/13335/v/600/depositphotos_133351928-stock-illustration-default-placeholder-man-and-woman.jpg'),
-          const SizedBox(height: 16),
-          const Text(
-            'Anggun Riyanto',
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
+      body: Center(
+        child: Column(
+          children: [
+            Image.asset('assets/images/fotoku.jpg'),
+            const SizedBox(height: 16),
+            const Text(
+              'Anggun Riyanto',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-          const SizedBox(height: 8),
-          const Text(
-            'Graphic Designer | Guru Pesantren',
-            style: TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.w400,
+            const SizedBox(height: 8),
+            const Text(
+              'Graphic Designer | Guru Pesantren',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w400,
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
